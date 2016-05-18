052183031ff39d156af026aa467008b922c177a3
Max <mszhidkov@gmail.com>
2016-05-18 13:20:49 +0300

    test commit 1

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -54,6 +54,7 @@ class SiteController extends Controller
 
     public function actionLogin()
     {
+        var_dump('123');
         if (!\Yii::$app->user->isGuest) {
             return $this->goHome();
         }
